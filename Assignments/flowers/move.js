fe4b14801e42cbe14da27828128b8ac88519591e
Blin75 <blinagushaj75@gmail.com>
2023-12-28 13:42:39 +0100

    i made some changes

--- Assignments/flowers/move.js
+++ Assignments/flowers/move.js
@@ -5,3 +5,27 @@ onload = () => {
         clearTimeout(c);
     }, 1000);
 };
+
+<body>
+    <script>
+        alert('Hello');
+        alert('Welcome to this webpage');
+        alert('Have fun!');
+    </script>
+</body>
+
+
+onload = () => {
+    const c = setTimeout(() => {
+        document.body.classList.remove("not-loaded");
+        clearTimeout(c);
+    }, 1000);
+};
+
+
+onload = () => {
+    const c = setTimeout(() => {
+        document.body.classList.remove("not-loaded");
+        clearTimeout(c);
+    }, 1000);
+};
