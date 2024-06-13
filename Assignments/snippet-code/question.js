d6f1de06650b3fa5711cef25a76b21d08bae0e19
Blin75 <144045160+Blin75@users.noreply.github.com>
2024-06-13 12:21:22 +0200

    js

---
+++ Assignments/snippet-code/question.js
@@ -0,0 +1,22 @@
+let  allQuestions = [
+    {
+        question:'What does HMTML stand for ?',
+        answer: 'HyperText Markup Language',
+        options: [
+           'HyperText Making Language', 
+           'HyperText Markup Language',
+           'Hiphop Making Language',
+           'Hinting Markup Language'
+        ]
+    },
+    {
+        question:'Which protocol we use to secure our webpage ?',
+        answer: 'HTTPS',
+        options: [
+           'HTTP', 
+           'TCPIP',
+           'SSH',
+           'HTTPS'
+        ]
+    }
+];
